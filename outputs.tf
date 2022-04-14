70a1366b8c2416a14f909338492f09b8d3e935df
1Mill <erik.ekberg@outlook.com>
2022-04-13 20:12:31 -0700

    Scaffold module

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "data" {
+	description = "File content parsed into an object."
+	value = local.data
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "inline_delimiter" {
+	default = "="
+	type = string
+}
+variable "filename" {
+	type = string
+}
+variable "newline_delimiter" {
+	default = "\n"
+	type = string
+}
